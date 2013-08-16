b8b0971ea51be22cf3efe4874d45bb746abb61d7
sonesuke <iamsonesuke@gmail.com>
2013-08-16 18:59:09 +0900

    first commit

---
+++ src/Eval/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Testgen;
+
+namespace Eval
+{
+    class Program
+    {
+        static Dictionary<string, int> symbolTable = new Dictionary<string, int>();
+
+        static void Main(string[] args)
+        {
+            List<Parser.Exp> e = Testgen.Parser.testParse(
+                "d1 + d2 + d3"
+                ).ToList();
+            foreach (var a in e)
+            {
+                int i = Parse(a);
+
+            }
+
+            System.Console.ReadLine();
+
+        }
+
+        static int Parse(Testgen.Parser.Exp val)
+        {
+            var idval = val as Testgen.Parser.Exp.IDVal;
+            if (idval != null)
+            {
+                return Parse(idval);
+            }
+            var opval = val as Testgen.Parser.Exp.Op;
+            if (opval != null)
+            {
+                return Parse(opval);
+            }
+            return 12;
+        }
+
+        static int Parse(Testgen.Parser.Exp.IDVal val)
+        {
+            return symbolTable[val.Item];
+        }
+
+        static int Parse(Testgen.Parser.Exp.Op val)
+        {
+            switch (val.Item1)
+            {
+                case ":":
+                case "+":
+                    return EvaluatePlus(val);
+                default:
+                    return 0;
+            }
+        }
+
+        static int EvaluatePlus(Testgen.Parser.Exp.Op val)
+        {
+            return Parse(val.Item2) + Parse(val.Item3);
+            //return Parse(val.Item2) + Parse(val.Item3);
+
+        }
+    }
+}
---
+++ src/parser/parser.fs
@@ -0,0 +1,80 @@
+﻿module Testgen.Parser
+open FParsec
+open FParsec.CharParsers
+open FParsec.Primitives
+
+// AST
+type Exprs = Exp list
+and Exp =
+  | IDVal      of string
+  | Op         of string * Exp * Exp 
+  | Fail       of string
+
+
+// Parser
+let ws = spaces
+let ch c = skipChar c >>. ws
+
+// Term
+let symbol: Parser<string, unit> =
+  parse {
+    let! h = letter
+    let! t = manyChars (letter <|> digit <|> anyOf "-_")
+    return System.Char.ToString(h) + t
+    }
+
+let termParse : Parser<Exp, unit> =
+   parse { 
+     let! wd = symbol
+     return (IDVal wd)
+     } 
+
+
+//	OPP
+let opp = new OperatorPrecedenceParser<Exp, unit, unit>()
+let expr = opp.ExpressionParser
+let term = (termParse .>> ws) <|> between (ch '(') (ch ')') expr
+opp.TermParser <- term
+opp.AddOperator(InfixOperator("=", ws, 
+                         2, 
+                         Associativity.Left, 
+                         fun x y -> (Op ("=" , x , y))))
+opp.AddOperator(InfixOperator("+", ws, 
+                         5, 
+                         Associativity.Left, 
+                         fun x y -> (Op ("+" , x , y)))) 
+opp.AddOperator(InfixOperator("*", ws, 
+                         6, 
+                         Associativity.Left, 
+                         fun x y -> (Op ("*" , x , y))))
+opp.AddOperator(InfixOperator("<-", ws, 
+                         6, 
+                         Associativity.Left, 
+                         fun x y -> (Op ("<-" , x , y))))
+
+
+let mainParser: Parser<Exp, unit> =
+  parse { 
+    let! ast = expr
+    let! _ = manyChars (anyOf ";")
+    return ast
+    }
+
+let cm: Parser<Exp list, unit> =
+  parse { 
+    do! spaces
+    let! ast = sepEndBy mainParser spaces
+    do! eof
+    return ast
+    }
+
+    //
+// ParserResult<Exp list> から、Exp list を取り出す
+let extractExprs x =
+  match x with
+  | Success (x, _, _) -> x
+  | Failure (x,y,_) -> failwith x
+
+
+let testParse prg = 
+  ((run cm) prg) |> extractExprs
